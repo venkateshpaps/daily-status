48b3c8ce6e8332e59da7aa4ad9228914bdc924bd
Venkatesh S <venkatesh_sai@photonint.com>
2018-01-19 16:22:49 +0530

    message JAN 19

---
+++ Basic Webpack/webpack.config.js
@@ -0,0 +1,26 @@
+var path =require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        inline: true,
+        port: 3000
+        },
+entry:'./index.js',
+output:{path:__dirname, filename: 'bundle.js'},
+module:{
+loaders:[
+{
+test:/.jsx?$/,
+loader:'babel-loader',
+exclude:/node_modules/,
+query:{
+presets:['es2015','react']
+}
+}
+
+]
+
+
+},
+};
